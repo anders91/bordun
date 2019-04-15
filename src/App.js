de21f89d99b5796f66e156ddc18cc74de4eff56c
Martin Lindahl <MartinLindahl@Martins-MacBook-Pro-2.local>
2019-04-15 14:38:57 +0200

    martins teständringar

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Edit <code>src/App.js</code> and save to reload. Hej hej här är martins ändringar
           </p>
           <a
             className="App-link"
