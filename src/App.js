2c147ae23b43f9575f4ff9d91b3ea6304b397530
Anders Lindahl <Anders@Anderss-MacBook-Pro.local>
2019-04-15 14:51:31 +0200

    Ander testar ändring

--- src/App.js
+++ src/App.js
@@ -11,6 +11,9 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload. Hej hej här är martins ändringar
           </p>
+          <p>
+            Anders ändrar här!
+          </p>
           <a
             className="App-link"
             href="https://reactjs.org"
