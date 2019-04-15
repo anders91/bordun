74f9cbd1278a5054912bde91dc2399e69dab7e7e
Martin Lindahl <MartinLindahl@Martins-MacBook-Pro-2.local>
2019-04-15 15:16:22 +0200

    tone import

--- src/App.js
+++ src/App.js
@@ -1,31 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './reset.css'
+import './App.css'
+import Tongenerator from './Tongenerator'
 
-class App extends Component {
+export default class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload. Hej hej här är martins ändringar
-          </p>
-          <p>
-            Anders ändrar här!
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Tongenerator />
+        hej på dig
       </div>
-    );
+    )
   }
 }
 
-export default App;
+
---
+++ src/Tongenerator.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import Tone from 'tone'
+
+export default class Tongenerator extends Component {
+    constructor() {
+        super()
+        console.log(Tone)
+    }
+  render() {
+    return (
+      <div>
+        Tongenerator
+      </div>
+    )
+  }
+}
