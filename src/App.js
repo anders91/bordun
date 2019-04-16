19fec28d56f27c502c8eb201dfd12cfcf6c1d269
Anders Lindahl <Anders@Anderss-MacBook-Pro.local>
2019-04-16 11:58:14 +0200

    tongenerator byter ton

--- src/App.js
+++ src/App.js
@@ -7,20 +7,13 @@ import Tongenerator from './Tongenerator';
 export default class App extends Component {
   constructor(props) {
     super(props)
-  
+    
     this.state = {
-      isplaying: false,
-      noteplaying: 1,
-
-
+      notePlaying: 1,
+      noteOctave: 3,
     }
-  
 
-
-  }
-
-  handlePlaypause(){
-    console.log('handlePlaypause')
+    this.handleNotechange = this.handleNotechange.bind(this);
   }
 
   handleNotechange(event){
@@ -31,18 +24,38 @@ export default class App extends Component {
     switch(expression) {
       case 'minusett':
         // code block
+        this.setState(prevState => {
+          return {
+            notePlaying : prevState.notePlaying - 1
+          }
+        })
         console.log('SWICH misuett')
         break;
       case 'plusett':
         // code block
+        this.setState(prevState => {
+          return {
+            notePlaying : prevState.notePlaying + 1
+          }
+        })
         console.log('SWICH plusett')
         break;
         case 'minstolv':
         // code block
+        this.setState(prevState => {
+          return {
+            noteOctave : prevState.noteOctave - 1
+          }
+        })
         console.log('SWICH minustolv')
         break;
         case 'plustolv':
         // code block
+        this.setState(prevState => {
+          return {
+            noteOctave : prevState.noteOctave + 1
+          }
+        })
         console.log('SWICH plustolv')
         break;
       default:
@@ -54,16 +67,18 @@ export default class App extends Component {
 
 
     return (
-      <div>
-{//        <Tongenerator />
-}        
-<div className="greenborder vilkenton">{this.state.noteplaying}</div>
+      <div>      
+<div className="greenborder vilkenton">{this.state.notePlaying}</div>
+<div className="greenborder vilkenton">{this.state.noteOctave}</div>
+
 <div id="minusett" onClick={this.handleNotechange} className="greenborder uppnerknappar">Ton ner</div>
 <div id="plusett" onClick={this.handleNotechange} className="greenborder uppnerknappar">Ton upp</div>
 <div id="minstolv" onClick={this.handleNotechange} className="greenborder uppnerknappar">oktav ner</div>
 <div id="plustolv" onClick={this.handleNotechange} className="greenborder uppnerknappar">oktav upp</div>
-<Tongenerator className="greenborder playpauseknapp" />
-
+<Tongenerator className="greenborder playpauseknapp"
+              notePlaying={this.state.notePlaying}
+              noteOctave={this.state.noteOctave}
+            />
       </div>
     )
   }
--- src/Tongenerator.js
+++ src/Tongenerator.js
@@ -3,15 +3,17 @@ import Tone from 'tone'
 import PlayPause from './PlayPause'
 
 export default class Tongenerator extends Component {
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
         this.state = {
           playing: false
         };
+
+        this.noteArray = ["c3", "c#3", "d3", "d#3", "e3", "f3", "f#3", "g3", "g#3", "a3", "a#3", "b3"]
         this.synth = new Tone.AMSynth().toMaster();
         this.play = this.play.bind(this);
-    }
-    
+      }
+
     play(note) {
 
         if (this.state.playing === false) {
@@ -26,10 +28,12 @@ export default class Tongenerator extends Component {
 
 
   render() {
+    this.synth.setNote(this.noteArray[Number(this.props.notePlaying%11)])
     return (
-      <div onClick={() => this.play("d4")}>
+      <div onClick={() => this.play(this.noteArray[Number(this.props.notePlaying)])}>
         <PlayPause  />
       </div>
-    )
+      ) 
+
+    }
   }
-}
